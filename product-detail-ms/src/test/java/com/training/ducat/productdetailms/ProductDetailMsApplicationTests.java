4bead7074de8850b2e6e76531a3de8dba6490364
smeashwani <104685977+smeashwani@users.noreply.github.com>
2022-06-11 10:21:54 +0530

    Initial commit

---
+++ product-detail-ms/src/test/java/com/training/ducat/productdetailms/ProductDetailMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.ducat.productdetailms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDetailMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
