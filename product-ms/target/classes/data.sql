6e0ebb66f6641b68a0b6ba36f7b4276f91b7c0cc
smeashwani <104685977+smeashwani@users.noreply.github.com>
2022-07-03 10:23:19 +0530

    docker

--- product-ms/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Product (ID, NAME, DESCRIPTION ) VALUES(-11,  'Table', 'Table desc');
-INSERT INTO Product (ID, NAME, DESCRIPTION ) VALUES(-12,  'Chair', 'Chair desc');
-INSERT INTO Product (ID, NAME, DESCRIPTION ) VALUES(-13,  'Desktop', 'Desktop desc');
--- product-ms/target/classes/schema_backup.sql
+++
@@ -1,7 +0,0 @@
-create table Product
-(
-   id integer not null,
-   name varchar(255) not null,
-   description varchar(255),
-   primary key(id)
-);
